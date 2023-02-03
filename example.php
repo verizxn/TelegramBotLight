6e45ff96d6232a7965c1969939da46e85d591a7b
Verlzon <noavailable@github>
2023-02-03 15:06:09 +0100

    whoops

--- example.php
+++ example.php
@@ -5,8 +5,8 @@ require_once __DIR__.'/vendor/autoload.php';
 $tg = new Verlzon\Telegrambotlight\Telegram('token', __DIR__.'/logs');
 $update = $tg->getUpdate();
 
-if(isset($update['text']))
+if(isset($update['message']['text']))
 	$tg->request('sendMessage', [
-		'chat_id' => $update['chat']['id'],
-		'text' => ($update['text'] == '/start')?'Hello world!':'Command not found...',
+		'chat_id' => $update['message']['chat']['id'],
+		'text' => ($update['message']['text'] == '/start')?'Hello world!':'Command not found...',
 	]);
